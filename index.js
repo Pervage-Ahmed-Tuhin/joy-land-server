392b37496ba5ba2bfbbe3299e5fc90b264170746
Pervage-Ahmed-Tuhin <parvageahmedtuhin123456@gmail.com>
2024-04-27 21:25:47 +0600

    added the dynamic id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -40,6 +40,13 @@ async function run() {
             res.send(result);
 
         });
+
+        app.get('/tourists/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await touristSpotCollection.findOne(query);
+            res.send(result);
+        })
 
 
         app.post('/tourists', async (req, res) => {
