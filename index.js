ceb8eb023ae98847422e62c4db793ade519060a7
Pervage-Ahmed-Tuhin <parvageahmedtuhin123456@gmail.com>
2024-04-28 19:38:12 +0600

    Handled the delete query

--- index.js
+++ index.js
@@ -74,6 +74,15 @@ async function run() {
 
 
 
+        app.delete('/tourists/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await touristSpotCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
